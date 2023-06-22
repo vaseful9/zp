258ff49df6f37ccdf739ca0fa70287a1be0bd169
vaseful9 <127093167+vaseful9@users.noreply.github.com>
2023-06-22 15:01:34 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,7 @@
+echo "# zp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vaseful9/zp.git
+git push -u origin main
